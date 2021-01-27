03d0362956a2bd674c064efdd1543603689674f0
Thiago <thiago_murakami@hotmail.com>
2021-01-27 16:16:11 -0300

    styling component widget

--- narutoquiz/pages/index.js
+++ narutoquiz/pages/index.js
@@ -23,11 +23,34 @@ const QuizContainer = styled.div`
   }
 `;
 
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
 export default function Home() {
   return (
     <BackgroundImage>
       <QuizContainer>
-      teste
+      <Widget>
+        teste
+      </Widget>
       </QuizContainer>
     </BackgroundImage>
   );
