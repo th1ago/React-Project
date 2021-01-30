bf3af1ae919bac996497c50af01f15436bd30324
Thiago <thiago_murakami@hotmail.com>
2021-01-30 00:31:56 -0300

    quiz template

--- narutoquiz/pages/index.js
+++ narutoquiz/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
@@ -8,21 +7,11 @@ import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Button from '../src/components/Button';
 import Input from '../src/components/Input';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -53,10 +42,9 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
-            <form onSubmit={function (infosDoEvento) {
+            <form onSubmit={(infosDoEvento) => {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
-              console.log('Fazendo uma submissão por meio do react');
             }}
             >
               <Input
--- narutoquiz/pages/quiz.js
+++ narutoquiz/pages/quiz.js
@@ -1,13 +1,32 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
-// import { ThemeProvider } from 'styled-components';
-// import QuizScreen from '../../src/screens/Quiz';
-// import db from '../db.json';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 
 export default function QuizDaGaleraPage() {
   return (
-    <div>
-      teste
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo>
+          <Widget>
+            <Widget.Header>
+              <h3>
+                Pergunta
+                1
+                de
+                {`${db.questions.length}`}
+              </h3>
+            </Widget.Header>
+            <img alt="descricao" style={{ width: '111%', height: '151px' }} src="https://placehold.it/411x411" />
+            <Widget.Content>
+              <h2>Titulo</h2>
+              <p>Descricao</p>
+            </Widget.Content>
+          </Widget>
+        </QuizLogo>
+      </QuizContainer>
+    </QuizBackground>
   );
 }
